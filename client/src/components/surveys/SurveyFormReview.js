51ed927c5a406d49c20f80c289b32639e5690941
Eduardo Reche <edu.reche@gmail.com>
2017-08-29 16:28:27 -0300

    survey form

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
+
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>
+          {label}
+        </label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      {reviewFields}
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
+        Back
+      </button>
+      <button className="green btn-flat white-text right">
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    formValues: state.form.surveyForm.values
+  };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
